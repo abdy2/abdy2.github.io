6fbe76f1d7f2e770624aab68d7e979c1ea3d39c1
abdy2 <113189359+abdy2@users.noreply.github.com>
2022-09-25 15:48:43 +0300

    Delete anim directory

--- anim/play.js
+++
@@ -1,8 +0,0 @@
-var animation = bodymovin.loadAnimation({
-    container: document.getElementById('bm'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path:'https://assets5.lottiefiles.com/packages/lf20_17p4evft.json'
-})
-console.log('connected to play')
